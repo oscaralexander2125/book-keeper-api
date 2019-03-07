038c6afc7f84eb23e27eb048be77e6d7f31c5ab9
Oscar A. Reyes <oscarreyes2194@gmail.com>
2019-03-07 12:21:07 -0600

    add test

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
 app.listen(process.env.PORT || 8080);
+
+module.exports = {app}
